53f241684c3f0bf6de2b0f43fa682eef68932634
Tapiwa Gwede <gwedet@usf.edu>
2021-06-26 17:19:31 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assignment_3_Web_look_and_feel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
